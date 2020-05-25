181b115c4f024550422c5042f67a8c9af40f29f8
vjanz <valon.januzaj98@gmail.com>
2020-05-25 17:25:10 +0200

    Subcategory Seeder class

---
+++ database/seeds/SubCategorySeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SubCategory;
+class SubCategorySeeder extends Seeder
+{
+
+    public function run()
+    {
+        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory1']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory2']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory3']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory4']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory5']);
+
+
+    }
+}
