07a5198ffd9bbfb89597d2925e3ac33b5143fb2a
Rron <rronjk@gmail.com>
2020-05-25 15:02:36 +0200

    Updated the resposnes

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 class SubCategoryController extends Controller
 {
 
-
     public function store(Request $request)
     {
         $this->validate($request,[
@@ -22,15 +21,22 @@ class SubCategoryController extends Controller
             'category_id' => $request->category_id
         ]);
         $subCategory->save();
+        $subCategoryFiltred = new SubCategory();
+        $subCategoryFiltred->name = $subCategory->name;
+        $subCategoryFiltred->id = $subCategory->id;
+        $subCategoryFiltred->category_id = $subCategory->category_id;
+
         return response()->json([
-            'message' => 'Successfully created SubCategory!'
+            'Message' => 'Successfully created SubCategory!',
+            'Subcategory' => $subCategoryFiltred
         ], 201);
     }
+
     public function findByCategory(Category $category)
     {
         $subCategories = $category->load("subcategories");
         $subFiltred = $subCategories["subcategories"];
-        return $subFiltred;
+        return response()->json($subFiltred, 201);
     }
 
     public function show($subcategory){
