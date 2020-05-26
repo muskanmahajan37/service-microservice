8caa0dcc65d6f98ec25883ce44e374b08065e12e
vjanz <valon.januzaj98@gmail.com>
2020-05-26 21:45:23 +0200

    Added function to get all subcategories

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -9,6 +9,14 @@ use Illuminate\Http\Request;
 class SubCategoryController extends Controller
 {
 
+    public function index()
+    {
+        $subcategories = SubCategory::all();
+        return response()->json(
+            $subcategories, 201
+        );
+    }
+
     public function store(Request $request)
     {
         $this->validate($request, [
