7095f0e5ba96f3941687e74e573e6390b5c8ba6a
Taulant Dema <taulantdema12@gmail.com>
2020-05-28 17:40:57 +0200

    Service seeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
-            SubCategorySeeder::class
+            SubCategorySeeder::class,
+            ServiceSeeder::class
+
         ]);
     }
 }
