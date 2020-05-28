12f841dadbaceae27546fba7c001915362a45cf0
Taulant Dema <taulantdema12@gmail.com>
2020-05-28 17:41:18 +0200

    SubCategorySeeder update

--- database/seeds/SubCategorySeeder.php
+++ database/seeds/SubCategorySeeder.php
@@ -7,59 +7,120 @@ class SubCategorySeeder extends Seeder
 {
     public function run()
     {
-        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>1, 'name'=>'Subcategory5']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Laravel']);
+        SubCategory::create(['category_id'=>1, 'name'=>'PHP']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Lumen']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Java']);
+        SubCategory::create(['category_id'=>1, 'name'=>'React']);
+        SubCategory::create(['category_id'=>1, 'name'=>'HTML']);
+        SubCategory::create(['category_id'=>1, 'name'=>'JS']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Spring']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Spring boot']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Python']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Ruby']);
+        SubCategory::create(['category_id'=>1, 'name'=>'Rail']);
+        SubCategory::create(['category_id'=>1, 'name'=>'SQL']);
+        SubCategory::create(['category_id'=>1, 'name'=>'MySQL']);
+        SubCategory::create(['category_id'=>1, 'name'=>'MongoDB']);
+        SubCategory::create(['category_id'=>1, 'name'=>'IT']);
+        SubCategory::create(['category_id'=>1, 'name'=>'.Net']);
+        SubCategory::create(['category_id'=>1, 'name'=>'.C++']);
 
-        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>2, 'name'=>'Subcategory5']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Business Consulting']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Financial Consulting']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Presentations']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Career advice']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Flyer Distribution']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Lead Generation']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Virtual Assistant']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Data entry']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Product Research']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Project Management']);
+        SubCategory::create(['category_id'=>2, 'name'=>'HR Consulting']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Business Plan']);
+        SubCategory::create(['category_id'=>2, 'name'=>'Branding Services']);
 
-        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>3, 'name'=>'Subcategory5']);
 
-        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>4, 'name'=>'Subcategory5']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Voice Over']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Composers']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Singers']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Mixing']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Music Lessons']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Podcast Editing']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Songwriters']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Audiobook']);
+        SubCategory::create(['category_id'=>3, 'name'=>'DJ']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Audio Ads']);
+        SubCategory::create(['category_id'=>3, 'name'=>'Sound Effects']);
 
-        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>5, 'name'=>'Subcategory5']);
 
-        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>6, 'name'=>'Subcategory5']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Articles & Blog Posts']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Translation']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Online Language Lessons']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Brand Voice & Editing']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Book Writing']);
+        SubCategory::create(['category_id'=>4, 'name'=>'eBook Writing']);
+        SubCategory::create(['category_id'=>4, 'name'=>'UX Writing']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Resume']);
+        SubCategory::create(['category_id'=>4, 'name'=>'CV']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Technical Writings']);
+        SubCategory::create(['category_id'=>4, 'name'=>'Grant Writing']);
 
-        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>7, 'name'=>'Subcategory3']);
+
+        SubCategory::create(['category_id'=>5, 'name'=>'SEO']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Marketing Strategy']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Local SEO']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Content Marketing']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Video Marketing']);
+        SubCategory::create(['category_id'=>5, 'name'=>'E-Commerce marketing']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Music Promotion']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Programming promotion']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Podcast Marketing']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Domain Research']);
+        SubCategory::create(['category_id'=>5, 'name'=>'Public Relations']);
+
+        SubCategory::create(['category_id'=>6, 'name'=>'Logo']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Game Design']);
+        SubCategory::create(['category_id'=>6, 'name'=>'3D']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Graphics for Streamers']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Twitch Store']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Tattoo Design']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Pattern Design']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Web Design']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Mobile Design']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Brochure Design']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Banner Ads']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Product Design']);
+        SubCategory::create(['category_id'=>6, 'name'=>'Character Modeling']);
+
+
+        SubCategory::create(['category_id'=>7, 'name'=>'Financial Consulting']);
+        SubCategory::create(['category_id'=>7, 'name'=>'Economy Consulting']);
+        SubCategory::create(['category_id'=>7, 'name'=>'Market Research']);
         SubCategory::create(['category_id'=>7, 'name'=>'Subcategory4']);
         SubCategory::create(['category_id'=>7, 'name'=>'Subcategory5']);
 
-        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>8, 'name'=>'Subcategory5']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Online Lessons']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Fitness']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Craft']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Arts']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Relationship Advice']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Health']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Astrology']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Traveling']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Gaming']);
+        SubCategory::create(['category_id'=>8, 'name'=>'Spiritual']);
 
-        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory1']);
-        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory2']);
-        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory3']);
-        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory4']);
-        SubCategory::create(['category_id'=>9, 'name'=>'Subcategory5']);
+
+        SubCategory::create(['category_id'=>9, 'name'=>'Graphics']);
+        SubCategory::create(['category_id'=>9, 'name'=>'E-commerce']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Book']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Podcasting']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Influencers']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Real Estate']);
+        SubCategory::create(['category_id'=>9, 'name'=>'Architecture']);
+
+
 
 
 
