89332a0cc241142b067afe15f632bb3d3531f425
Taulant Dema <taulantdema12@gmail.com>
2020-05-29 23:07:17 +0200

    changes to ServiceController

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -14,8 +14,8 @@ use Illuminate\Facades\Storage;
 use DB;
 use PhpOption\None;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-//use  Symfony\Component\HttpFoundation\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 
 
 
@@ -38,35 +38,13 @@ class ServiceController extends Controller
             'price' => 'required',
             'description' => 'required'
         ]);
-//        return $request->image;
-//        dd($request->all());
-
-       return request()->all();
-        $image = $request->file('image');
-
-
-
-
-
-//        return $image;
-//        $new_name =$image->getClientOriginalExtension();
-//        $image->move(base_path('public/images'), $new_name);
-//        if ($request->hasFile('image')) {
-//            $filenameWithExt = $request->file('image')->getClientOriginalName();
-//            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-//            $extension = $request->file('image')->getClientOriginalExtension();
-//            $fileNameToStore = $filename . '_' . time() . '.' . $extension;
-//        } else {
-//            $fileNameToStore = 'noimage.jpg';
-//        }
-
         $service = new Service([
             'name' => $request->name,
             'category_id' => $request->category_id,
             'user_id' => $request->user_id,
             'price' => $request->price,
             'description' => $request->description,
-            'image' => $new_name,
+            'image' =>$request->image,
             'subcategory_id' => $request->subcategory_id,
         ]);
         $service->save();
@@ -77,34 +55,18 @@ class ServiceController extends Controller
         ], 201);
     }
 
-    public function show(Service $service)
-    {
-     return $service;
+    public function show(Service $service){
+        return $service;
     }
-
-//    public function findByUser(Request $request)
-//    {
-//        $id = $request->get('id');
-//        return Service::where('user_id', $id)->get();
-//    }
 
     public function edit(Request $request, $id)
     {
         $this->validate($request, [
             'name' => 'required|string',
             'category_id' => 'required',
-            //            'user_id'=>'required',
             'price' => 'required',
             'description' => 'required'
         ]);
-        if ($request->hasFile('image')) {
-            $filenameWithExt = $request->file('image')->getClientOriginalName();
-            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            $extension = $request->file('image')->getClientOriginalExtension();
-            $fileNameToStore = $filename . '_' . time() . '.' . $extension;
-        } else {
-            $fileNameToStore = 'noimage.jpg';
-        }
 
         $service = new Service([
             'name' => $request->name,
@@ -112,7 +74,7 @@ class ServiceController extends Controller
             'user_id' => $request->user_id,
             'price' => $request->price,
             'description' => $request->description,
-            'image' => $fileNameToStore,
+            'image' =>$request->image,
 
         ]);
         $service = Service::find($id);
