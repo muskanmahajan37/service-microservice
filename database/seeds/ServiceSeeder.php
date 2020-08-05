74820f83f3ce59660e7a4267b597cfacd0038874
vjanz <valon.januzaj98@gmail.com>
2020-08-05 22:32:30 +0200

    Seeders for presentation

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\Service;
+use Illuminate\Database\Seeder;
 
 class ServiceSeeder extends Seeder
 {
@@ -12,26 +12,52 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>1,'name'=>'I will design stunning wordpress website using divi theme', 'image'=>'wordpress-1.jpg','price'=>22,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>8,'name'=>'I will design and develop a full stack website', 'image'=>'full-stack.jpg','price'=>11,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>2,'name'=>'I will improve your current wordpress website', 'image'=>'wordpress-2.jpg','price'=>44,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>3,'name'=>'I will design and create a fully responsive website with divi theme', 'image'=>'wordpress-3.jpg','price'=>44,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>4,'name'=>'I will install wordpress, theme setup like demo, do customization', 'image'=>'wordpress-4.jpg','price'=>54,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>1,'name'=>'I will fix bugs in your ios app', 'image'=>'swift-1.jpg','price'=>11,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>5,'name'=>'I will implement a functionality or method in swift', 'image'=>'swift-2.jpg','price'=>25,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>6,'name'=>'I will develop ios and android mobile app using react native', 'image'=>'react-native.jpg','price'=>70,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>7,'name'=>'I will develop ios app for iphone and ipad in swift 5', 'image'=>'swift-3.jpg','price'=>55,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>1,'name'=>'I will do android and ios apps in flutter mobile app development', 'image'=>'flutter-1.jpg','price'=>35,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>5,'name'=>'I will help you in your xcode swift project', 'image'=>'swift-4.jpg','price'=>22,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>6,'name'=>'I will be your android app developer', 'image'=>'android-1.jpg','price'=>16,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>2,'name'=>'I will fix bugs and develop xamarin forms apps', 'image'=>'xcode-1.jpg','price'=>20,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>2,'name'=>'I will design and develop modern website with latest technologies', 'image'=>'web-1.jpg','price'=>20,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>2,'name'=>'I will fix, code, develop, debug your HTML website or HTML5 website', 'image'=>'web-2.jpg','price'=>20,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>2,'name'=>'I will do python bots, scraper, automators, and more', 'image'=>'web-3.jpg','price'=>20,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>2,'name'=>'I will develop web app using react js, vue js, node js, php', 'image'=>'web-4.jpg','price'=>20,'description'=>'Some Description']);
-        Service::create(['user_id'=>1,'category_id'=>1,'username'=>'user','subcategory_id'=>2,'name'=>'I will be your full stack developer', 'image'=>'full-stack-1.jpg','price'=>20,'description'=>'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 1, 'name' => 'I will design stunning wordpress website using divi theme', 'image' => 'wordpress-1.jpg', 'price' => 22, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 8, 'name' => 'I will design and develop a full stack website', 'image' => 'full-stack.jpg', 'price' => 11, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 2, 'name' => 'I will improve your current wordpress website', 'image' => 'wordpress-2.jpg', 'price' => 44, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 3, 'name' => 'I will design and create a fully responsive website with divi theme', 'image' => 'wordpress-3.jpg', 'price' => 44, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 4, 'name' => 'I will install wordpress, theme setup like demo, do customization', 'image' => 'wordpress-4.jpg', 'price' => 54, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 1, 'name' => 'I will fix bugs in your ios app', 'image' => 'swift-1.jpg', 'price' => 11, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 5, 'name' => 'I will implement a functionality or method in swift', 'image' => 'swift-2.jpg', 'price' => 25, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 6, 'name' => 'I will develop ios and android mobile app using react native', 'image' => 'react-native.jpg', 'price' => 70, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 7, 'name' => 'I will develop ios app for iphone and ipad in swift 5', 'image' => 'swift-3.jpg', 'price' => 55, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 1, 'name' => 'I will do android and ios apps in flutter mobile app development', 'image' => 'flutter-1.jpg', 'price' => 35, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 5, 'name' => 'I will help you in your xcode swift project', 'image' => 'swift-4.jpg', 'price' => 22, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 6, 'name' => 'I will be your android app developer', 'image' => 'android-1.jpg', 'price' => 16, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 2, 'name' => 'I will fix bugs and develop xamarin forms apps', 'image' => 'xcode-1.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 2, 'name' => 'I will design and develop modern website with latest technologies', 'image' => 'web-1.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 2, 'name' => 'I will fix, code, develop, debug your HTML website or HTML5 website', 'image' => 'web-2.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 2, 'name' => 'I will do python bots, scraper, automators, and more', 'image' => 'web-3.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 2, 'name' => 'I will develop web app using react js, vue js, node js, php', 'image' => 'web-4.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 1, 'category_id' => 1, 'username' => 'user', 'subcategory_id' => 2, 'name' => 'I will be your full stack developer', 'image' => 'full-stack-1.jpg', 'price' => 20, 'description' => 'Some Description']);
 
+        Service::create(['user_id' => 2, 'category_id' => 2, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will write essay and article on finance, marketing, accounting, and economyr', 'image' => 'economy-1.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 2, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will provide you with investment stocks in current economy', 'image' => 'economy-2.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 2, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will provide you with investment stocks in current economy', 'image' => 'economy-3.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 2, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will write quality business,finance and economy articles', 'image' => 'economy-4.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 2, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will be your accountant for your finances of your company', 'image' => 'economy-5.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 2, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will be your full stack developer', 'image' => 'economy-6.jpg', 'price' => 20, 'description' => 'Some Description']);
 
+        Service::create(['user_id' => 2, 'category_id' => 4, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will write original and high quality articles', 'image' => 'writing-1.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 4, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will write viral article upto 1000 words for you in 6 days', 'image' => 'writing-2.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 4, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will write viral article upto 1000 words for you in 6 days', 'image' => 'writing-3.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 4, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will create your professional engineering resume', 'image' => 'writing-4.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 4, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will do SEO article writing for you ', 'image' => 'writing-5.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 4, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will write a captivating SEO blog or engaging website copy', 'image' => 'writing-6.jpg', 'price' => 20, 'description' => 'Some Description']);
+
+        Service::create(['user_id' => 2, 'category_id' => 5, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will set up your facebook and instagram ad campaigns', 'image' => 'marketing-1.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 5, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will set up your facebook and instagram ad campaigns', 'image' => 'marketing-2.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 5, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will set up your facebook and instagram ad campaigns', 'image' => 'marketing-3.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 5, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will set up your facebook and instagram ad campaigns', 'image' => 'marketing-4.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 5, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will set up your facebook and instagram ad campaigns', 'image' => 'marketing-5.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 5, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will set up your facebook and instagram ad campaigns', 'image' => 'marketing-6.jpg', 'price' => 20, 'description' => 'Some Description']);
+
+        Service::create(['user_id' => 2, 'category_id' => 3, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will design an outstanding logo', 'image' => 'music-1.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 3, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will music an outstanding logo', 'image' => 'music-2.jpg', 'price' => 20, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 3, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will music 2 modern logo music', 'image' => 'music-3.jpg', 'price' => 50, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 3, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'Our Studio will music modern and minimalist logo for your business', 'image' => 'music-4.jpg', 'price' => 42, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 3, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will create a flat minimalist logo music in 24 hours', 'image' => 'music-5.jpg', 'price' => 33, 'description' => 'Some Description']);
+        Service::create(['user_id' => 2, 'category_id' => 3, 'username' => 'admin', 'subcategory_id' => 20, 'name' => 'I will music 2 modern minimalist logo free vector files', 'image' => 'music-6.jpg', 'price' => 13, 'description' => 'Some Description']);
 
     }
 }
